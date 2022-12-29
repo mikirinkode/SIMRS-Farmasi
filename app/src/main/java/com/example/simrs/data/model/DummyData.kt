af2c2a9ce7c446d6351462b4e6f2725cd88347cf
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-12-29 17:24:52 +0700

    create model

---
+++ app/src/main/java/com/example/simrs/data/model/DummyData.kt
@@ -0,0 +1,13 @@
+package com.example.simrs.data.model
+
+object DummyData {
+    fun getDummyIndustries(): List<Industry> = listOf(
+        Industry(10001, "Kimia Farma", "JL. Serayu Wetan", "Semarang", "082356278162"),
+        Industry(10002, "Sanbe Farma", "JL. Serayu Wetan", "Semarang", "082356278162"),
+        Industry(10003, "Graha Farma", "JL. Serayu Wetan", "Semarang", "082356278162"),
+        Industry(10004, "Capri Farma", "JL. Serayu Wetan", "Semarang", "082356278162"),
+        Industry(10005, "PT. Borneo Medika", "JL. Serayu Wetan", "Semarang", "082356278162"),
+        Industry(10006, "PT", "JL. Serayu Wetan", "Semarang", "082356278162"),
+        Industry(10007, "Kimia Farma", "JL. Serayu Wetan", "Semarang", "082356278162"),
+    )
+}
---
+++ app/src/main/java/com/example/simrs/data/model/Supplier.kt
@@ -0,0 +1,11 @@
+package com.example.simrs.data.model
+
+data class Supplier(
+    val code: Int,
+    var name: String,
+    var address: String,
+    var city: String,
+    var phoneNumber: String,
+    var bankName: String,
+    var backAccountNumber: String,
+)
